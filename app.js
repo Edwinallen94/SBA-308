bc75cf1c8780d8225b41355a27341b97c5902ca1
Edwin <Edwinallen94@aol.com>
2024-09-16 14:10:56 -0400

    Commented out the commented out instructions and started the function with the parameters

---
+++ app.js
@@ -0,0 +1,25 @@
+// Employ basic JavaScript syntax accurately.
+// Implement control flow structures such as conditionals and loops effectively.
+// Use arrays and objects to organize and manage data.
+// Develop functions to create reusable code.
+// Utilize loops and iteration to navigate through data collections.
+// Implement error handling to manage potential code failures gracefully.
+
+//You will create a script that gathers data, processes it,
+//and then outputs a consistent result as described by a specification.
+//This is a very typical situation in industry, and this particular scenario has been modified from a real application.
+//The data you will use is provided below.
+
+//steps to start the function
+// 1. Need to create a function that GATHERS Data that outputs the results
+// 2. Four different data types (courseInfo, assignmentGroup, assignmentInfo and learnerSubmission)
+// 3. should throw an error (try/catch??)
+
+function dataGatherer(
+  courseInfo,
+  assignmentGroup,
+  assignmentInfo,
+  learnerSubmission
+) {
+  //adding each data types as parmaters
+}
